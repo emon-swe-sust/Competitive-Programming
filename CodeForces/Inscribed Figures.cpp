2668b1c77204fdb43f2c395b7b7e04d0b99643aa
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-11-01 00:04:53 +0600

    Create Inscribed Figures.cpp

---
+++ CodeForces/Inscribed Figures.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,i,j,k,ans=0;
+    cin >> n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=1;i<n;i++)
+    {
+        if((arr[i-1] == 2 && arr[i] == 3) || (arr[i-1] == 3 && arr[i] == 2))
+        {
+            cout << "Infinite" << endl;
+            return 0;
+        }
+        if((arr[i-1]==1 && arr[i]==2) ||(arr[i-1]==2 && arr[i]==1))
+            ans = ans+3;
+        if((arr[i-1]==1 && arr[i] == 3) || (arr[i-1]==3 && arr[i] == 1))
+            ans = ans+4;
+    }
+    cout << "Finite" << endl;
+    cout << ans << endl;
+    return 0;
+}
