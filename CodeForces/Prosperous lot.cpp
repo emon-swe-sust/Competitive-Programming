908b0886c9fa3bc674885b6126507a71e0810bb3
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-11-01 00:09:53 +0600

    Create Prosperous lot.cpp

---
+++ CodeForces/Prosperous lot.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n,i,j;
+    cin >> n;
+    if(n > 36)
+    {
+        cout << -1 <<endl;
+        return 0;
+    }
+    for(i=n;i>2;i=i-2)
+    {
+        cout << 8;
+    }
+    if(i == 2)
+        cout << 8;
+    else
+        cout << 9;
+        return 0;
+}
