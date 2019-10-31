43c89b3173aaee291704b14befd40b3287ba269e
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-11-01 00:07:27 +0600

    Create Points on the line.cpp

---
+++ CodeForces/Points on the line.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+   int a[105]={0},bal=0,d,n,i,ans=0,j,hi = INT_MIN,k;
+   cin >> n >> d;
+   for(i=1;i<=n;i++)
+   {
+        cin >> k;
+        a[k]++;
+ 
+   }
+   a[0]=0;
+   for(i=1;i<=100;i++)
+   {
+       if(a[i] != 0)
+        bal++;
+       a[i]+=a[i-1];
+   }
+   if(bal == 1)
+   {
+       cout << 0 ;
+       return 0;
+   }
+  /* for(i=1;i<=100;i++)
+    cout << a[i] << " ";
+   cout << endl;*/
+   for(i=1;i<=100-d;i++)
+   {
+ 
+       if(hi<(a[i+d]-a[i-1]))
+       {
+          hi = a[i+d]-a[i-1];
+         // cout << i+d << " " << a[i+d] << " " << i <<  " " << a[i] << " " << hi << endl;
+       }
+   }
+   if(hi<0)
+    cout << 0;
+   else
+    cout << n-hi;
+    return 0;
+}
