7822fc66959e8284a29f6217e482159c4f2ae756
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2020-01-09 23:28:06 +0600

    to test

--- CodeForces/Long Number.cpp
+++ CodeForces/Long Number.cpp
@@ -4,49 +4,30 @@ using namespace std;
 int main()
 {
     int n,i,j,k,l=0;
-    string in,out="";
-    map<int,int>mp;
+    char from,to;
+    string in;
+    bool bo = false;
+    int mp[11];
     cin >> n >> in;
     for(i=0;i<9;i++)
     {
         cin >> k ;
         mp[i+1]=k;
     }
-    for(i=1;i<=9;i++)
-    {
-       // cout << mp[i] << " " << i << endl;
-        if(mp[i]!=i)
-            l=1;
-    }
-    if(l==0)
-    {
-        cout << in << endl;
-        return 0;
-    }
     for(i=0;i<n;i++)
     {
-        out=out+char(mp[int(in[i]-'0')]+'0');
-    }
-    int chk = 0;
-    for(i=0;i<n;i++)
-    {
-        if(out[i]-'0' > in[i]-'0')
+        if((int(in[i]-'0') < mp[int(in[i]-'0')]))
         {
-            chk=1;
-            break;
+            bo=true;
         }
-        else if(in[i]-'0'>out[i]-'0')
+        if(bo)
         {
-            cout << in[i] << " " << out[i] << endl;
-            chk=2;
-            break;
+            if(int(in[i]-'0') <= mp[int(in[i]-'0')])
+                in[i]=char(mp[int(in[i]-'0')]+'0');
+            else
+                break;
         }
     }
-    if(chk == 0)
-        cout << in << endl;
-    else if(chk==1)
-        cout << out << endl;
-    else
-        cout << in << endl;
+    cout << in << endl;
     return 0;
 }
--- CodeForces/Points on the Line.cpp
+++ CodeForces/Points on the Line.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
    int a[105]={0},bal=0,d,n,i,ans=0,j,hi = INT_MIN,k;
@@ -9,7 +9,7 @@ int main()
    {
         cin >> k;
         a[k]++;
-
+ 
    }
    a[0]=0;
    for(i=1;i<=100;i++)
@@ -28,7 +28,7 @@ int main()
    cout << endl;*/
    for(i=1;i<=100-d;i++)
    {
-
+ 
        if(hi<(a[i+d]-a[i-1]))
        {
           hi = a[i+d]-a[i-1];
