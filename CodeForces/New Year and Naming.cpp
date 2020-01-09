19878c6277e8645591918e5516f46f0037067e55
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2020-01-09 23:45:32 +0600

    push

--- CodeForces/New Year and Naming.cpp
+++ CodeForces/New Year and Naming.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
+    int i,j,n,m,q,k;
+    cin >> n >> m;
+    string str1[n+1],str2[m+1],str[n*m+1];
 
+    for(i=1;i<=n;i++)
+        cin >> str1[i];
+    for(i=1;i<=m;i++)
+        cin >> str2[i];
+
+    cin >> q;
+
+    for(i=0;i<q;i++)
+    {
+        cin >> k;
+        if(k%n == 0)
+            cout << str1[n];
+        else
+            cout << str1[k%n];
+        if(k%m == 0)
+            cout << str2[m] << endl;
+        else
+            cout << str2[k%m] << endl;
+    }
 
     return 0;
 }
